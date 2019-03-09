c5b5d39f787f3ff9594b056a92f902bd73ba6218
Brijesh Patel <Brij@Brijesh-Patels-MacBook-Pro.local>
2019-03-09 01:42:56 -0500

    added youtube videos link

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,10 @@
-
-
+var APIKey = "5258a1b12eab1eb6a43101023288eacf";
+var youtubeId ="";
+var link ="";
 $(document).ready(function(){
-
-    var APIKey = "5258a1b12eab1eb6a43101023288eacf";
-
+    $("#youtube").attr("src",link);
     // Here we are building the URL we need to query the database
-   // var queryURL = "https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=" + APIKey;
-    var queryURL = "https://api.themoviedb.org/3/genre/movie/list?api_key="+APIKey+"&language=en-US";
+    var queryURL = "https://api.themoviedb.org/3/movie/now_playing?api_key="+APIKey+"&language=en-US&page=1";
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
       url: queryURL,
@@ -14,25 +12,53 @@ $(document).ready(function(){
     })
       // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
-        
-        
-        // Log the queryURL
-        console.log(queryURL);
 
         // Log the resulting object
-        console.log(response.genres[0]);
-        for(var i=0;i<19;i++){
-        var newDiv=$("<div>");
-        var newButton=$("<button>");
-        $(".buttonBox").append(newDiv);
-        newDiv.append(newButton);
-        newButton.text(response.genres[i].name);
-        newButton.attr("id","genres");
-        newButton.addClass("col-md-12 btn btn-outline-primary");
-        newButton.attr("data-id",response.genres[i].id);
+        console.log(response.results[0].original_language);
+        var count =0;
+        for(var i=0;i<10+count;i++)
+        {
+                if(response.results[i].original_language==="en")   
+                {
+                var newDiv=$("<div>");
+                var newButton=$("<button>");
+                $(".buttonBox").append(newDiv);
+                newDiv.append(newButton);
+                newButton.text(response.results[i].original_title);
+                newButton.attr("id","nowPlaying"+i);
+                newButton.addClass("col-md-12 btn btn-outline-primary nowPlaying");
+                newButton.attr("data-id",response.results[i].id);
+                }else{
+                    count++;
+                }
         }
 
       });
 
 
+      $(document).on("click",".nowPlaying",function(event){
+        
+        console.log(event);
+        var target = event.currentTarget.id;
+        var dataId = $("#"+target).attr("data-id");
+        console.log(dataId);
+        var queryURL= "https://api.themoviedb.org/3/movie/"+dataId+"/videos?api_key="+APIKey+"&language=en-US";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            .then(function(response) {
+
+                console.log(queryURL);
+              console.log(response.results[0].key);  
+              youtubeId = response.results[0].key;    
+              link = "https://www.youtube.com/embed/"+youtubeId+"?autoplay=1";
+            });
+            
+            $("#youtube").attr("src",link);
+           // document.getElementsByName("source").innerHTML = link;
+      });
+
+    
 });
